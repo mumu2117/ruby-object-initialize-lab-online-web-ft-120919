70a9a306ecf45dea5911060ec99272672ecb6236
Jennifer Miller <jmiller@archersystems.us>
2020-01-03 23:01:25 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,26 @@
 class Dog
 
-    def initialize(name, breed = "Mutt")
-        @name = name
-        @breed = breed
-    end
+  def initialize(name, breed="Mutt")
+    @name = name
+    @breed = breed
+  end
+
+  def name=(name)
+    @name = name
+  end
+
+  def name
+    @name
+
+  end
+
+  def breed=(breed)
+    @breed = breed
+  end
+  
+  def breed
+    @breed
+  end
+
+
 end
