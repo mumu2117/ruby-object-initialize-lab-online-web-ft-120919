c7b267eb8a7956bfa76edc5450bcfdf6e918696f
Jennifer Miller <jmiller@archersystems.us>
2020-01-03 23:04:19 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,26 +1,12 @@
 class Dog
+attr_accessor :name, :breed 
 
   def initialize(name, breed="Mutt")
     @name = name
     @breed = breed
   end
 
-  def name=(name)
-    @name = name
-  end
 
-  def name
-    @name
-
-  end
-
-  def breed=(breed)
-    @breed = breed
-  end
-  
-  def breed
-    @breed
-  end
 
 
 end
